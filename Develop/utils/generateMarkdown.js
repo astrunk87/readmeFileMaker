0087129e595c8724cda83e4292fd7cb171062eec
astrunk87 <134554632+astrunk87@users.noreply.github.com>
2023-08-16 18:32:06 -0500

    grabbed some code that was being worked on during office hours

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,6 +1,17 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+
+  if (license === 'none')
+  return "";
+} else {
+  return '[!github]'(http://img.shields.io/badge/license-${liscense}-blue.svg)
+}
+
+}
+
+// ^grabbed from office hours work 
+
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
